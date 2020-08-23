8ee238b108ebec4566330cb6be3972643f4058ff
gurmeet <garryuw@gmail.com>
2020-08-23 11:00:10 -0700

    unit test

--- src/main/java/com/apple/App.java
+++ src/main/java/com/apple/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello APPLE PAY From Maven " );
+        System.out.println( "Hello APPLE PAY From Maven... " );
     }
 }
